7b23ff41e13bf067027f86fe760fa226f6268543
Mauro2912 <asanchez2912@gmail.com>
2019-09-01 00:49:42 -0500

    Complete SeedDB class and modify the program class

---
+++ MyVet.Web/Data/SeedDB.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyVet.Web.Data.Entities;
+
+namespace MyVet.Web.Data
+{
+    public class SeedDB
+    {
+        private readonly DataContext _context;
+
+        public SeedDB(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            await CheckPetTypesAsync();
+            await CheckServiceTypesAsync();
+            await CheckOwnersAsync();
+            await CheckPetsAsync();
+            await CheckAgendasAsync();
+        }
+
+        private async Task CheckPetTypesAsync()
+        {
+            if (!_context.PetTypes.Any())
+            {
+                _context.PetTypes.Add(new PetType { Name = "Cat" });
+                _context.PetTypes.Add(new PetType { Name = "Dog" });
+                _context.PetTypes.Add(new PetType { Name = "Turtle" });
+                _context.PetTypes.Add(new PetType { Name = "Snake" });
+                _context.PetTypes.Add(new PetType { Name = "Horse" });
+                _context.PetTypes.Add(new PetType { Name = "Donkey" });
+                _context.PetTypes.Add(new PetType { Name = "Hamster" });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckPetsAsync()
+        {
+            var owner = _context.Owners.FirstOrDefault();
+            var petType = _context.PetTypes.FirstOrDefault();
+
+            if (!_context.Pets.Any())
+            {
+                AddPet("Leonardo", owner, petType, "green turtle");
+                AddPet("Casimiro", owner, petType, "Catalan donkey");
+                AddPet("Samsom", owner, petType, "American Pitbull");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckOwnersAsync()
+        {
+            if (!_context.Owners.Any())
+            {
+                AddOwner("8989898", "Juan", "Zuluaga", "234 3232", "310 322 3221", "Calle Luna Calle Sol");
+                AddOwner("7655544", "Jose", "Cardona", "343 3226", "300 322 3221", "Calle 77 #22 21");
+                AddOwner("6565555", "Maria", "López", "450 4332", "350 322 3221", "Carrera 56 #22 21");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckServiceTypesAsync()
+        {
+
+            if (!_context.ServiceTypes.Any())
+            {
+                _context.Add(new ServiceType { Name = "Medical consultation" });
+                _context.Add(new ServiceType { Name = "Emergency" });
+                _context.Add(new ServiceType { Name = "vaccination" });
+                _context.Add(new ServiceType { Name = "Cutting and brushing" });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckAgendasAsync()
+        {
+            if (!_context.Agendas.Any())
+            {
+                var initialDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0);
+                var finalDate = initialDate.AddMonths(1);
+                while (initialDate < finalDate)
+                {
+                    if (initialDate.DayOfWeek != DayOfWeek.Sunday)
+                    {
+                        var finalDate2 = initialDate.AddHours(10);
+                        while (initialDate < finalDate2)
+                        {
+                            _context.Agendas.Add(new Agenda
+                            {
+                                Date = initialDate.ToUniversalTime(),
+                                IsAvailable = true
+                            });
+
+                            initialDate = initialDate.AddMinutes(30);
+                        }
+
+                        initialDate = initialDate.AddHours(14);
+                    }
+                    else
+                    {
+                        initialDate = initialDate.AddDays(1);
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+            }
+        }    
+
+    private void AddOwner(string document, string firstName, string lastName, string fixedPhone, string cellPhone, string address)
+        {
+            _context.Owners.Add(new Owner
+            {
+                Address = address,
+                CellPhone = cellPhone,
+                Document = document,
+                FirstName = firstName,
+                FixedPhone = fixedPhone,
+                LastName = lastName
+            });
+        }
+
+        private void AddPet(string name, Owner owner, PetType petType, string race)
+        {
+            _context.Pets.Add(new Pet
+            {
+                Born = DateTime.Now.AddYears(-2),
+                Name = name,
+                Owner = owner,
+                PetType = petType,
+                Race = race
+            });
+        }
+    }
+}
