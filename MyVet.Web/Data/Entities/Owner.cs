47c2523d0a78901f6e29cc87f36f664e33725425
Mauro2912 <asanchez2912@gmail.com>
2019-08-31 22:48:15 -0500

    Add DataContext

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display (Name = "First Name" )]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Display(Name = "Cell Name")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Owner")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+
+    }
+}
