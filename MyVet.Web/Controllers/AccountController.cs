caa94eaf9fb72fb32c079e97d8b1c11986a2b8ad
Mauro2912 <asanchez2912@gmail.com>
2019-09-01 11:06:29 -0500

    Initial loginViewModel

---
+++ MyVet.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public AccountController()
+        {
+            
+        }
+    }
+}
