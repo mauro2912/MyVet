94a7471d760e39f680573de1fb3e01cb081417c5
Mauro2912 <asanchez2912@gmail.com>
2019-09-01 00:11:55 -0500

    CompleteDB, add Agenda, History,Pet, Pettype and serviceType classes

--- MyVet.Web/Data/Entities/Owner.cs
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -10,30 +10,30 @@ namespace MyVet.Web.Data.Entities
     {
         public int id { get; set; }
 
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Document { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Display (Name = "First Name" )]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Display(Name = "Fixed Phone")]
         public string FixedPhone { get; set; }
 
-        [Required]
-        [MaxLength(20)]
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Display(Name = "Cell Name")]
         public string CellPhone { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Address { get; set; }
 
         [Display(Name = "Owner")]
@@ -42,6 +42,7 @@ namespace MyVet.Web.Data.Entities
         [Display(Name = "Owner")]
         public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
 
-
+        public ICollection<Pet> Pets { get; set; }
+        public ICollection<Agenda> Agendas { get; set; }
     }
 }
